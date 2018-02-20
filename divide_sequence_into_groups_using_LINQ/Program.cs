b84ba3130e2f1bfb643f6081daab1594533aa82a
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-20 21:46:17 +1100

    Initial commit

---
+++ divide_sequence_into_groups_using_LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * C# Program to Divide Sequence into Groups using LINQ
+ */
+using System;
+using System.Linq;
+using System.IO;
+
+namespace divide_sequence_into_groups_using_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seq = Enumerable.Range(100, 100).Select(x => x / 10f);
+            var grps = from x in seq.Select((i, j) => new { i, Grp = j / 10 })
+                       group x.i by x.Grp into y
+                       select new { Min = y.Min(), Max = y.Max() };
+            foreach (var grp in grps)
+                Console.WriteLine("Min: " + grp.Min + " Max:" + grp.Max);
+            Console.ReadLine();
+        }
+    }
+}
